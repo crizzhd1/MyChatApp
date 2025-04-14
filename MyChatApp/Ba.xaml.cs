f52de22e3367573107003f7b0b74c6d0b2f8e272
crizzhd1 <djralph1@hotmail.com>
2025-04-14 16:36:46 -0400

    an flash

--- MyChatApp/Ba.xaml.cs
+++ MyChatApp/Ba.xaml.cs
@@ -1,9 +1,34 @@
+
 namespace MyChatApp;
 
 public partial class Ba : ContentPage
 {
-	public Ba()
+	bool isToggled = false;
+
+    public Ba()
 	{
 		InitializeComponent();
 	}
+
+    async void ToggelLight(object sender, EventArgs e)
+	{
+        isToggled = !isToggled;
+
+        try
+        {
+            if (isToggled)
+            {
+                await Flashlight.Default.TurnOffAsync();
+
+            }
+            else
+            {
+                await Flashlight.Default.TurnOnAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }

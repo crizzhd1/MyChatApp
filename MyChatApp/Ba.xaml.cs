261fed65353f87a37fc0d3c29f7874a351de8bdd
crizzhd1 <djralph1@hotmail.com>
2025-04-29 23:59:12 -0400

    added a Accelerometer with text fore mobile not what i wanted so will change it

--- MyChatApp/Ba.xaml.cs
+++ MyChatApp/Ba.xaml.cs
@@ -11,14 +11,14 @@ public partial class Ba : ContentPage
 	}
 
     async void ToggelLight(object sender, EventArgs e)
-	{
+    {
         isToggled = !isToggled;
 
         try
         {
             if (isToggled)
             {
-                
+
                 await Flashlight.Default.TurnOnAsync();
 
             }
@@ -31,5 +31,31 @@ public partial class Ba : ContentPage
         {
             await DisplayAlert("Error", ex.Message, "OK");
         }
+    }
+
+    public void ToggleAccelerometer(object sender, EventArgs e)
+    {
+        if (Accelerometer.Default.IsSupported)
+        {
+            if (!Accelerometer.Default.IsMonitoring)
+            {
+                // Turn on accelerometer
+                Accelerometer.Default.ReadingChanged += Accelerometer_ReadingChanged;
+                Accelerometer.Default.Start(SensorSpeed.UI);
+            }
+            else
+            {
+                // Turn off accelerometer
+                Accelerometer.Default.Stop();
+                Accelerometer.Default.ReadingChanged -= Accelerometer_ReadingChanged;
+            }
+        }
+    }
+
+    private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+    {
+        // Update UI Label with accelerometer state
+        WAlk.TextColor = Colors.Green;
+        WAlk.Text = $"Accel: {e.Reading}";
     }
 }
